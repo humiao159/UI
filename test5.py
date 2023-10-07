d5e506ad46f4acd5846114a6af5ef4f0002320f2
humiao159 <1160435259@qq.com>
2023-10-07 15:24:20 +0800

    ssss

---
+++ test5.py
@@ -0,0 +1,16 @@
+import requests
+
+# 定义接口的URL
+url = 'https://jsonplaceholder.typicode.com/posts/1'  # 这是一个示例URL，请根据你的需求修改
+
+# 发送GET请求
+response = requests.get(url)
+
+# 检查响应状态码
+if response.status_code == 200:
+    # 解析JSON响应
+    data = response.json()
+    print("接口响应数据：")
+    print(data)
+else:
+    print(f"接口请求失败，状态码：{response.status_code}")
