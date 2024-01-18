d359ad8891dccb76d2cc17d0b8fddf3e4744c533
humiao <1160435259@qq.com>
2024-01-19 01:14:34 +0800

    111

--- test4.py
+++ test4.py
@@ -1,10 +1,11 @@
 
 from selenium import webdriver
 import time
-
+from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 print("开始测试")
-driver.get("https://www.dltkh.top/")
+driver.get("https://www.baidu.com/")
+driver.find_element(By.ID,'kw').send_keys('taobao')
 print("测试完成")
 driver.maximize_window()
 time.sleep(6)
